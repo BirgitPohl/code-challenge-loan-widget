9967882bcda76ddb78e30747e5b6679119f487ad
Birgit Pohl <trash.birgitpohl@gmail.com>
2019-08-31 21:15:08 +0200

    Vue setup

---
+++ src/components/LoanCalculationWidget.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loan-widget">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class LoanCalculationWidget extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <LoanCalculationWidget msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import LoanCalculationWidget from '@/components/LoanCalculationWidget.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    LoanCalculationWidget,
+  },
+})
+export default class Home extends Vue {}
+</script>
